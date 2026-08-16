548ace7342ed5c5254a4b39c59741545380200cd
Dipanshu Chaturvedi <197403975+dipanshu564@users.noreply.github.com>
2026-08-16 14:01:30 +0530

    Update next.config.ts

--- mainsuper50/next.config.ts
+++ mainsuper50/next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  outputFileTracingRoot: path.join(__dirname),
+
   typescript: {
     ignoreBuildErrors: true,
   },
-  // 禁用 Next.js 热重载，由 nodemon 处理重编译
+
   reactStrictMode: false,
+
   eslint: {
-    // 构建时忽略ESLint错误
     ignoreDuringBuilds: true,
   },
 };
